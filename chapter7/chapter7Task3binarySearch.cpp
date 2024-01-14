695c06e0b406df6ded7654e4cbc473b5d9b313ae
ShadonPC <55762492+Artjxm@users.noreply.github.com>
2024-01-14 09:22:50 +0300

    Chapter 7, task 3.b done. 3.a was an iterative (and less problematic) approach, 3.b is recursive.

--- chapter7/chapter7Task3binarySearch.cpp
+++ chapter7/chapter7Task3binarySearch.cpp
@@ -8,11 +8,11 @@ int average(int min, int max) {
 
 // вспомогательная функция поиска наименьшей степени двойки, превышающей число
 int maxIteration(const int value) {
-    int power(1), sought(0);
+    int power(1), notFound(0);
     while (true) {
-        if (power >= value) return sought;
+        if (power >= value) return notFound;
         power <<= 1;
-        ++sought;
+        ++notFound;
     }
 }
 
@@ -30,25 +30,60 @@ int binarySearchIterative(int* array, int target, int min, int max) {
     int max_i = maxIteration(max - min + 1);
 
     for (int i = min; i < max_i; ++i) {
-        int avg = average(min, max);
-        if (array[avg] > target) max = avg;
-        else if (array[avg] < target) min = avg;
-        else return avg;
+        int midPoint = average(min, max);
+        if (array[midPoint] > target) max = midPoint;
+        else if (array[midPoint] < target) min = midPoint;
+        else return midPoint;
     }
     return -1;
+}
+
+int binarySearchRecursive(int* array, const int target, const int min, const int max) {
+    int notFound(-1);
+
+    // проверка искомого вне диапазона массива
+    if (target < array[min] || target > array[max])
+        return notFound;
+
+    const int midPoint = average(min, max);
+
+    if (array[midPoint] > target) return binarySearchRecursive(array, target, min, midPoint - 1);
+    else if (array[midPoint] < target) return binarySearchRecursive(array, target, midPoint + 1, max);
+    else if (array[midPoint] == target) return midPoint;
+    else return notFound;
+}
+
+// авто-тест работы функции, а то лень вводить каждый раз
+void test(int* array) {
+    int testArr[]{3, 4, 7, 9, 13, 15, 19, 22, 24, 28, 33, 37, 41, 43, 45, 47, 50, 51};
+
+    for (const auto&i: testArr) {
+        const int index = binarySearchRecursive(array, i, 0, 14);
+        if (array[index] == i && index != -1)
+            std::cerr << i << ":\tpassed.";
+        else
+            std::cerr << i << ":\tfailed";
+
+        if (i == 3 || i == 45 || i == 51)
+            std::cerr << " as it should be.";
+
+        std::cerr << '\n';
+    }
 }
 
 void chapter7Task3binarySearch() {
     int array[] = {4, 7, 9, 13, 15, 19, 22, 24, 28, 33, 37, 41, 43, 47, 50};
 
-    std::cout << "Enter a number:";
-    int x;
-    std::cin >> x;
+    // std::cout << "Enter a number:";
+    // int x;
+    // std::cin >> x;
 
-    const int index = binarySearchIterative(array, x, 0, 14);
+    // const int index = binarySearchRecursive(array, x, 0, 14);
 
-    if (array[index] == x && index != -1)
-        std::cout << "Good! Your value " << x << " is on position " << index << " in array!\n";
-    else
-        std::cout << "Fail! Your value " << x << " isn't in array!\n";
+    // if (array[index] == x && index != -1)
+    //     std::cout << "Good! Your value " << x << " is on position " << index << " in array!\n";
+    // else
+    //     std::cout << "Fail! Your value " << x << " isn't in array!\n";
+
+    test(array);
 }
