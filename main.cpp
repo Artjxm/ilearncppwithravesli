4ea2b7e357e33954521199503f49a327f866012c
ShadonPC <55762492+Artjxm@users.noreply.github.com>
2024-01-26 19:05:51 +0300

    cleanup

--- main.cpp
+++ main.cpp
@@ -1,5 +1,3 @@
-#include <cassert>
-
 #include "chapter5/chapter5Task1.h"
 #include "chapter5/chapter5Task2.h"
 #include "chapter6/chapter6Task1.h"
@@ -9,6 +7,7 @@
 #include "chapter6/chapter6Task6.h"
 #include "chapter7/calculatorUsingFunctionPointers.h"
 #include "chapter7/chapter7Task3binarySearch.h"
+#include <algorithm>
 
 void chapter5() {
     chapter5Task1();
@@ -28,57 +27,10 @@ void chapter7() {
     chapter7Task3binarySearch();
 }
 
-class Stack {
-    int m_array[10];
-    int m_length{0};
 
-public:
-    void reset() {
-        m_length = 0;
-        for (auto&i: m_array) {
-            i = 0;
-        }
-    }
-
-    bool push(const int value) {
-        if (m_length == 10)
-            return false;
-
-        m_array[m_length++] = value;
-        return true;
-    }
-
-    int pop() {
-        assert(m_length > 0 && "Error trying to pop an element from empty stack");
-        return m_array[--m_length];
-    }
-
-    void print() {
-        std::cout << "( ";
-        for (int i = 0; i < m_length; ++i)
-            std::cout << m_array[i] << ' ';
-        std::cout << ")\n";
-    }
-};
 
 int main() {
-    Stack stack;
-    stack.reset();
 
-    stack.print();
-
-    stack.push(3);
-    stack.push(7);
-    stack.push(5);
-    stack.print();
-
-    stack.pop();
-    stack.print();
-
-    stack.pop();
-    stack.pop();
-
-    stack.print();
 
     return 0;
 }
