7877804b5bd3fdcb7719cec684ecbebda13c069f
Artjom <artjom.moskovka@gmail.com>
2024-03-02 11:40:31 +0300

    cleanup

--- main.cpp
+++ main.cpp
@@ -1,15 +1,7 @@
-#include <iostream>
 
-#include "chapter13/Pair.h"
 
 int main() {
-    StringValuePair<int> svp("Amazing", 7);
 
-    std::cout << "Pair: " << svp.first() << ' ' << svp.second() << '\n';
-
-    StringValuePair<double> svp2("Mild", 2.5);
-
-    std::cout << "Pair: " << svp2.first() << ' ' << svp2.second() << '\n';
 
     return 0;
 }
