993dc8fcccb747f6a3488cc0863aa0b73232b393
Artjom <artjom.moskovka@gmail.com>
2024-03-04 09:46:50 +0300

    cleanup

--- main.cpp
+++ main.cpp
@@ -1,20 +1,7 @@
-#include "chapter14/Fraction.h"
+
 
 int main() {
-    std::cout << "Enter the numerator:";
-    int numerator;
-    std::cin >> numerator;
-
-    std::cout << "Enther the denominator:";
-    int denominator;
-    std::cin >> denominator;
-
-    try {
-        Fraction fraction(numerator, denominator);
-        fraction.print();
-    } catch (std::exception &exception) {
-        std::cout << exception.what();
-    }
+    
 
     return 0;
 }
