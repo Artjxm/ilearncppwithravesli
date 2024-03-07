ccd6dbe2dced8a3788f67c1dd0ed8a5df5208a6e
Artjom <artjom.moskovka@gmail.com>
2024-03-07 14:35:51 +0300

    Chapter 15, lesson 193 (std::unique_ptr with std::make_unique() practice)

--- main.cpp
+++ main.cpp
@@ -1,22 +1,38 @@
 #include <iostream>
-#include <string>
-#include <vector>
+#include <memory>
+
+class Fraction {
+    int m_numerator = 0;
+    int m_denominator = 1;
+
+public:
+    explicit Fraction(int numerator = 0, int denominator = 1) : m_numerator(numerator),
+                                                       m_denominator(denominator) {}
+
+    friend std::ostream& operator<<(std::ostream& out, const Fraction& f1) {
+        out << f1.m_numerator << "/" << f1.m_denominator;
+        return out;
+    }
+};
+
+void printFraction(const Fraction* const ptr) {
+    if (ptr) std::cout << *ptr;
+}
 
 int main() {
-    std::vector<std::string> v;
-    std::string str("bye");
+    const auto ptr(std::make_unique<Fraction>(7, 9));
 
-    std::cout << "Copying str\n";
-    v.push_back(str);
-
-    std::cout << "str: " << str << '\n';
-    std::cout << "v: " << v.back() << '\n';
-
-    std::cout << "Moving str\n";
-    v.push_back(std::move(str));
-
-    std::cout << "str: " << str << '\n';
-    std::cout << "v: " << v.at(0) << ' ' << v.at(1) << '\n';
+    printFraction(ptr.get());
 
     return 0;
 }
+
+/*
+ * Задание №1:
+ * В данном случае умный указатель не сможет предотвратить очистку памяти, если сам будет
+ * некорректно удален
+ * Ответ автора: Умные указатели в качестве членов класса удаляют свой ресурс только в том случае,
+ * если объект класса выходит из области видимости. Если мы выделим объект класса динамически и не
+ * удалим его должным образом, то объект класса никогда не выйдет из области видимости, и умный
+ * указатель не сможет очистить ресурс, который он хранит.
+ */
