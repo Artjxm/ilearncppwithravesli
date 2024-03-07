bd65034188b32722b21da3f01a88ed2c2fc82349
Artjom <artjom.moskovka@gmail.com>
2024-03-07 09:32:40 +0300

    Chapter 15, lesson 192 (std::move() practice)

--- main.cpp
+++ main.cpp
@@ -1,62 +1,22 @@
 #include <iostream>
-
-#include "Timer.h"
-
-template<class T>
-class DynamicArray {
-    T* m_array;
-    int m_length;
-
-public:
-    DynamicArray(int length) : m_array(new T[length]), m_length(length) {}
-
-    ~DynamicArray() { delete[] m_array; }
-
-    DynamicArray(const DynamicArray& arr) = delete;
-
-    DynamicArray& operator=(const DynamicArray& arr) = delete;
-
-    DynamicArray(DynamicArray&& arr)  noexcept : m_array(arr.m_array), m_length(arr.getLength()) {
-        arr.m_length = 0;
-        arr.m_array = nullptr;
-    }
-
-    DynamicArray& operator=(DynamicArray&& arr)  noexcept {
-        if (&arr == this) return *this;
-
-        delete[] m_array;
-
-        m_length = arr.getLength();
-        m_array = arr.m_array;
-
-        arr.m_length = 0;
-        arr.m_array = nullptr;
-
-        return *this;
-    }
-
-    int getLength() const { return m_length; }
-    T& operator[](int index) { return m_array[index]; }
-    const T& operator[](int index) const { return m_array[index]; }
-};
-
-DynamicArray<int> cloneArrayAndDouble(const DynamicArray<int>& arr) {
-    DynamicArray<int> dbl(arr.getLength());
-    for (int i(0); i < arr.getLength(); ++i) dbl[i] = arr[i] * 2;
-
-    return dbl;
-}
+#include <string>
+#include <vector>
 
 int main() {
-    Timer t;
+    std::vector<std::string> v;
+    std::string str("bye");
 
-    DynamicArray<int> arr(100'000);
+    std::cout << "Copying str\n";
+    v.push_back(str);
 
-    for (int i = 0; i < arr.getLength(); i++) arr[i] = i;
+    std::cout << "str: " << str << '\n';
+    std::cout << "v: " << v.back() << '\n';
 
-    arr = cloneArrayAndDouble(arr);
+    std::cout << "Moving str\n";
+    v.push_back(std::move(str));
 
-    std::cout << t.elapsed();
+    std::cout << "str: " << str << '\n';
+    std::cout << "v: " << v.at(0) << ' ' << v.at(1) << '\n';
 
     return 0;
 }
